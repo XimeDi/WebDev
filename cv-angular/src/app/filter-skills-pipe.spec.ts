3b937cb303e4e39df3a41b3cc86660da070e6888
XimeDi <ximenadiaz@ufm.edu>
2025-09-24 03:10:47 -0600

    Componente Skills

---
+++ cv-angular/src/app/filter-skills-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterSkillsPipe } from './filter-skills-pipe';
+
+describe('FilterSkillsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterSkillsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- cv-angular/src/app/skills/skills.ts
+++ cv-angular/src/app/skills/skills.ts
@@ -2,10 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
-  templateUrl: './skills.html',
-  styleUrl: './skills.css'
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.css']
 })
-export class Skills {
+export class SkillsComponent {
+  searchTerm: string = '';
 
+  technologies: string[] = [
+    'Github',
+    'Docker',
+    'Postman',
+    'AWS EC2',
+    'Matplotlib'
+  ];
+
+  languages: string[] = [
+    'Python',
+    'Java',
+    'C',
+    'SQL'
+  ];
+
+  softSkills: string[] = [
+    'Solución de problemas',
+    'Pensamiento crítico',
+    'Adaptabilidad y aprendizaje continuo',
+    'Comunicación efectiva',
+    'Trabajo en equipo y colaboración'
+  ];
+
+  // La lógica de filtrado se maneja con una pipe
 }
