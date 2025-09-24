7dbb8151bd3bb86520aa516c58d4e7887a8a47f1
XimeDi <ximenadiaz@ufm.edu>
2025-09-24 03:15:16 -0600

    Componente Experience

--- cv-angular/src/app/experience/experience.ts
+++ cv-angular/src/app/experience/experience.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
-  templateUrl: './experience.html',
-  styleUrl: './experience.css'
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css']
 })
-export class Experience {
+export class ExperienceComponent {
+  showExperience: boolean = true;
 
+  experience = [
+    { period: 'Agosto 2025 - Noviembre 2025', company: 'ES Consulting', position: 'Practicante' },
+    { period: 'Mar 2024 - Jun 2024', company: 'Tech Solutions Inc.', position: 'Asistente de Desarrollo' }
+  ];
+
+  toggleExperience() {
+    this.showExperience = !this.showExperience;
+  }
 }
