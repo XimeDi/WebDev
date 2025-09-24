b209d97b650f48a770636a7c2d267a3f138075b6
XimeDi <ximenadiaz@ufm.edu>
2025-09-24 03:13:12 -0600

    Componente Education

--- cv-angular/src/app/education/education.ts
+++ cv-angular/src/app/education/education.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-education',
-  imports: [],
-  templateUrl: './education.html',
-  styleUrl: './education.css'
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
 })
-export class Education {
-
+export class EducationComponent {
+  educationData = [
+    {
+      year: '2021-Actualidad',
+      institution: 'Universidad Francisco Marroquín',
+      title: 'Baccalaureus Scientiae y Licenciado en Ingeniería en Ciencias de la Computación con especialización en Ciencia de los Datos'
+    },
+    {
+      year: '2020',
+      institution: 'Centro de Estudios Americano CEA y Kinder Mundo',
+      title: 'Bachiller en Ciencias y Letras'
+    }
+  ];
 }
